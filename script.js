2a47e13589b5a53a1157efe1108d9e18d966c611
briannamonsegue <43378962+briannamonsegue@users.noreply.github.com>
2019-01-07 10:17:18 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("#button1").click(function(){
+ $(".img1").hide();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
